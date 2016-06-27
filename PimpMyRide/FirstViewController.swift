a405c8be21d0b0ddef97efbcabd120822a37452f
Yassin AGHANI <aghani.yassin@gmail.com>
2016-06-27 13:28:25 +0200

    Initial Commit

---
+++ PimpMyRide/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  PimpMyRide
+//
+//  Created by Yassin Aghani on 27/06/16.
+//  Copyright © 2016 Yassin Aghani. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ PimpMyRide/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  PimpMyRide
+//
+//  Created by Yassin Aghani on 27/06/16.
+//  Copyright © 2016 Yassin Aghani. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
